1d9700d64a0d0b9bbba8f9de1f1a68ada2996f62
Alexey Abrosimov <amberlex78@gmail.com>
2017-05-18 23:13:40 +0300

    added migrations: parking_adv_table

---
+++ database/migrations/2017_05_18_200023_parking_adv_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ParkingAdvTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parking_adv', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('adv_id')->unsigned()->default(1);
+            $table->integer('parking_id')->unsigned()->default(1);
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+
+            $table->foreign('adv_id')->references('id')->on('adv');
+            $table->foreign('parking_id')->references('id')->on('parking');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('parking_adv', function (Blueprint $table) {
+            //
+        });
+    }
+}
